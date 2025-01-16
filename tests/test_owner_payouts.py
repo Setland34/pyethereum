9963618da93247021cc1a716c0ab1a86e0953740
Seth Landry <waymakerijn@outlook.com>
2025-01-15 23:00:08 -0600

    Add tests for owner payouts and balance updates
    * Add `test_owner_payout_on_transaction` to verify owner payout during transaction processing
    * Add `test_owner_payout_on_receive` to verify owner payout during transaction reception
    * Add `test_owner_payout_on_multiple_transactions` to verify owner payout with multiple transactions
    * Update `setUp` method to initialize sender, receiver, and owner addresses and balances
    * Remove old test cases for owner payouts

--- tests/test_owner_payouts.py
+++ tests/test_owner_payouts.py
@@ -1,51 +1,42 @@
 import unittest
 from blocks import Block
 from transactions import Transaction
-from manager import mainblk, genaddr
+from manager import receive
+from processblock import eval
 
 class TestOwnerPayouts(unittest.TestCase):
     def setUp(self):
         self.block = Block()
-        self.priv, self.addr = genaddr("test")
-        self.block.set_balance(self.addr, 1000)
+        self.sender = '0x1234567890abcdef1234567890abcdef12345678'
+        self.receiver = '0xabcdefabcdefabcdefabcdefabcdefabcdefabcd'
+        self.owner = '0x7713974908be4bed47172370115e8b1219f4a5f0'
+        self.block.set_balance(self.sender, 1000)
+        self.block.set_balance(self.owner, 0)
 
-    def test_owner_payout(self):
-        tx = Transaction(0, '0x7713974908be4bed47172370115e8b1219f4a5f0', 100, 10, [])
-        tx.sign(self.priv)
-        mainblk.receive(tx.serialize())
-        self.assertEqual(self.block.get_balance('0x7713974908be4bed47172370115e8b1219f4a5f0'), 100)
-        self.assertEqual(self.block.get_balance(self.addr), 890)
+    def test_owner_payout_on_transaction(self):
+        tx = Transaction(0, self.receiver, 100, 10, [])
+        tx.sender = self.sender
+        self.block.transactions.append(tx)
+        eval(self.block, self.block.transactions, 0, self.block.coinbase)
+        self.assertEqual(self.block.get_balance(self.receiver), 100)
+        self.assertEqual(self.block.get_balance(self.owner), 100)
 
-    def test_owner_payout_with_fee(self):
-        tx = Transaction(0, '0x7713974908be4bed47172370115e8b1219f4a5f0', 100, 10, [])
-        tx.sign(self.priv)
-        mainblk.receive(tx.serialize())
-        self.assertEqual(self.block.get_balance('0x7713974908be4bed47172370115e8b1219f4a5f0'), 100)
-        self.assertEqual(self.block.get_balance(self.addr), 890)
+    def test_owner_payout_on_receive(self):
+        tx = Transaction(0, self.receiver, 100, 10, [])
+        tx.sender = self.sender
+        receive(tx.serialize())
+        self.assertEqual(self.block.get_balance(self.receiver), 100)
+        self.assertEqual(self.block.get_balance(self.owner), 100)
 
-    def test_multiple_owner_payouts(self):
-        tx1 = Transaction(0, '0x7713974908be4bed47172370115e8b1219f4a5f0', 100, 10, [])
-        tx1.sign(self.priv)
-        mainblk.receive(tx1.serialize())
-        tx2 = Transaction(1, '0x7713974908be4bed47172370115e8b1219f4a5f0', 50, 5, [])
-        tx2.sign(self.priv)
-        mainblk.receive(tx2.serialize())
-        self.assertEqual(self.block.get_balance('0x7713974908be4bed47172370115e8b1219f4a5f0'), 150)
-        self.assertEqual(self.block.get_balance(self.addr), 835)
-
-    def test_owner_payout_balance_update(self):
-        tx = Transaction(0, '0x7713974908be4bed47172370115e8b1219f4a5f0', 200, 20, [])
-        tx.sign(self.priv)
-        mainblk.receive(tx.serialize())
-        self.assertEqual(self.block.get_balance('0x7713974908be4bed47172370115e8b1219f4a5f0'), 200)
-        self.assertEqual(self.block.get_balance(self.addr), 780)
-
-    def test_owner_payout_insufficient_balance(self):
-        tx = Transaction(0, '0x7713974908be4bed47172370115e8b1219f4a5f0', 1100, 10, [])
-        tx.sign(self.priv)
-        mainblk.receive(tx.serialize())
-        self.assertEqual(self.block.get_balance('0x7713974908be4bed47172370115e8b1219f4a5f0'), 0)
-        self.assertEqual(self.block.get_balance(self.addr), 1000)
+    def test_owner_payout_on_multiple_transactions(self):
+        tx1 = Transaction(0, self.receiver, 100, 10, [])
+        tx1.sender = self.sender
+        tx2 = Transaction(1, self.receiver, 200, 20, [])
+        tx2.sender = self.sender
+        self.block.transactions.extend([tx1, tx2])
+        eval(self.block, self.block.transactions, 0, self.block.coinbase)
+        self.assertEqual(self.block.get_balance(self.receiver), 300)
+        self.assertEqual(self.block.get_balance(self.owner), 300)
 
 if __name__ == '__main__':
     unittest.main()
